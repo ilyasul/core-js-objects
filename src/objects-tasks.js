dda7d020059fdb68e9df786f6b5e94910efe072c
ilyasul <i.sul95@gmail.com>
2025-12-08 22:59:30 +0300

    feat: task "makeWorld" is done

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -142,8 +142,15 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const maxLength = Object.values(lettersObject).flat().sort().slice(-1)[0];
+  const arr = new Array(maxLength);
+  Object.entries(lettersObject).forEach(([key, value]) => {
+    value.forEach((keyOfVal) => {
+      arr[keyOfVal] = key;
+    });
+  });
+  return arr.join('');
 }
 
 /**
