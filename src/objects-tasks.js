fd931a4d19b0c50039664768214fde62d93f1b88
ilyasul <i.sul95@gmail.com>
2025-12-10 20:45:38 +0300

    feat: task "fromJSON" is done

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -234,8 +234,8 @@ function Rectangle(width, height) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { height: 10, width: 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -249,8 +249,13 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const res = Object.create(proto);
+  const obj = JSON.parse(json);
+  Object.entries(obj).forEach(([key, value]) => {
+    res[key] = value;
+  });
+  return res;
 }
 
 /**
